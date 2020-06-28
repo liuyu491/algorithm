f3520179675202a147ea240642e6ed55787da882
liuyu_hasee <liuyu49@outlook.com>
2020-06-28 14:47:50 +0800

    第一次提交：完成双路快排

--- src/algorithm/sort/QuickSort.java
+++ src/algorithm/sort/QuickSort.java
@@ -1,14 +1,29 @@
 package algorithm.sort;
 
+import datastructure.Array;
+
+import java.util.Arrays;
+
 public class QuickSort {
 
 
-    public void quickSort(Comparable[] list){
+    public static void quickSort(Comparable[] list){
+            quickSort(list,0,list.length-1);
+    }
 
+    private static void quickSort(Comparable[] list,int lo,int hi){
+        /*递归停止的条件*/
+        if(lo>=hi){
+            return;
+        }
+        int index = partition(list,lo,hi);
+        quickSort(list,lo,index-1);
+        quickSort(list,index+1,hi);
     }
 
     //双轴快排
-    private int partition(Comparable[] list, int lo,int hi){
+    private static int partition(Comparable[] list, int lo,int hi){
+
         //选取第一个位置的元素作为分隔元素
         Comparable v = list[lo];
         int i = lo;
@@ -16,12 +31,36 @@ public class QuickSort {
         //[lo,i]中的所有元素都小于等于v，[j,hi]中的所有元素都大于等于v。开始的时候集合中没有元素。
 
         while(true){
+            while(list[++i].compareTo(v)<0){
+                //防止越界
+                if(i==hi){
+                    break;
+                }
+            }
 
+            while(list[--j].compareTo(v)>0) {
+
+            }
+               if(i>=j) {
+                   break;
+               }
+            swap(list,i,j);
         }
-
-
-
-
+        swap(list,lo,j);
+        return j;
     }
 
-}
+        private static void swap(Comparable[] list,int i,int j){
+            Comparable tmp = list[i];
+            list[i] = list[j];
+            list[j] = tmp;
+        }
+
+    public static void main(String[] args) {
+        Integer[] list = {2,5,8,1,7,4,4,9,3,10};
+        quickSort(list);
+        System.out.println(Arrays.toString(list));
+    }
+    }
+
+
